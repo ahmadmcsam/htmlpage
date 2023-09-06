9f09ecc048dbbc4ebfe5cae8c08098ba4e5209b5
ahmadmcsam <142683066+ahmadmcsam@users.noreply.github.com>
2023-09-06 11:11:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Update Hostname
+    document.getElementById("hostname").textContent = window.location.hostname;
+
+    // Update IP Address
+    fetch('https://api64.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("ip").textContent = data.ip;
+        });
+
+    // Update Date and Time
+    const dateTimeElement = document.getElementById("datetime");
+    setInterval(function () {
+        const now = new Date();
+        dateTimeElement.textContent = now.toLocaleString();
+    }, 1000);
+
+    // Update Uptime
+    // You may need to customize this part based on your server environment.
+
+    // Update Browser Info
+    document.getElementById("browser").textContent = `Browser: ${navigator.userAgent}`;
+});
