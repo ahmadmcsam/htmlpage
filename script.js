f8ff170e45931ec96beedd3edb3d1bc9f9834606
ahmadmcsam <142683066+ahmadmcsam@users.noreply.github.com>
2023-09-06 12:17:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Update Hostname
-    document.getElementById("hostname").textContent = window.location.hostname;
+function updateDateTime() {
+    const datetimeElement = document.getElementById("datetime");
+    const now = new Date();
+    datetimeElement.textContent = now.toLocaleString();
+}
 
-    // Update IP Address
-    fetch('https://api64.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("ip").textContent = data.ip;
-        });
+// Update date and time every second
+setInterval(updateDateTime, 1000);
 
-    // Update Date and Time
-    const dateTimeElement = document.getElementById("datetime");
-    setInterval(function () {
-        const now = new Date();
-        dateTimeElement.textContent = now.toLocaleString();
-    }, 1000);
-
-    // Update Uptime
-    // You may need to customize this part based on your server environment.
-
-    // Update Browser Info
-    document.getElementById("browser").textContent = `Browser: ${navigator.userAgent}`;
-});
+// Initial update
+updateDateTime();
