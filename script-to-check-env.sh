1a992242f5ddaa930be1c07be7a22ff3b0ee4fd5
Ahmad Raza <101821827+noscopev6@users.noreply.github.com>
2023-09-11 21:58:12 +0530

    1

---
+++ script-to-check-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Source the external environment variable file
+source ./.env
+
+# Rest of your script that uses these variables
+echo "My variable is: $MY_VARIABLE"
+echo "Another variable is: $ANOTHER_VARIABLE"
