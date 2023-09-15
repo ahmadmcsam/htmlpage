b5fa55ab5a5788c5643646de6ead7ed3b631e67e
Ahmad Raza <101821827+noscopev6@users.noreply.github.com>
2023-09-15 17:56:20 +0530

    Script Update on Fri 15 Sep 2023 05:56:20 PM IST

---
+++ auto-push-github.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BRANCH_NAME="original_code"
+# Define the local directory containing your code
+CODE_DIRECTORY="/home/mcsam/Documents/samplefiles/htmlpage"
+
+# Navigate to your code directory
+cd "$CODE_DIRECTORY" || exit
+
+# Add, commit, and push your changes to the specified branch
+git add .
+git commit -m "Script Update on $(date)"
+git push origin "$BRANCH_NAME"
+
+echo "Code has been pushed to GitHub branch: $BRANCH_NAME"
